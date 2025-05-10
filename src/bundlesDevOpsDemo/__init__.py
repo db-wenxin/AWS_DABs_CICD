7d6ee4f91a987c49a98398b84847548e9fc8094d
wenxin-liu_data <wenxin.liu@databricks.com>
2025-05-10 15:18:15 -0400

    Update dlt function

--- src/bundlesDevOpsDemo/__init__.py
+++ src/bundlesDevOpsDemo/__init__.py
@@ -1,13 +1,13 @@
 __version__ = "0.0.1"
 
-# Direct alias for the main module to support both import patterns
-import sys
-from . import main as _temp_main_module
+# Import everything from main module
+from .main import *
 
-# Replace main function reference with module reference
-# This is critical for the DLT pipeline which uses main.get_taxis()
-sys.modules[__name__].main = _temp_main_module
+# Create a special get_taxis function directly in the main object
+# This is a hack to support the DLT pipeline which uses main.get_taxis()
+def get_taxis(spark):
+    from .main import get_taxis as original_get_taxis
+    return original_get_taxis(spark)
 
-# Also expose entry point function for Databricks jobs
-def main():
-    return _temp_main_module.main()
+# Add the get_taxis function to the main function as an attribute
+main.get_taxis = get_taxis
